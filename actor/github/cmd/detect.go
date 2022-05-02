52c2d76685366379e19c9568a05fdfdde7d49f63
Takuya TAKAHASHI <takutaka@pepabo.com>
2022-05-02 03:51:23 +0000

    add Execute

--- actor/github/cmd/detect.go
+++ actor/github/cmd/detect.go
@@ -5,20 +5,23 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"log"
+
 	"github.com/spf13/cobra"
+	"github.com/takutakahashi/oci-image-operator/actor/github/pkg/detect"
 )
 
 // detectCmd represents the detect command
 var detectCmd = &cobra.Command{
 	Use:   "detect",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		d, err := detect.NewDetect("/tmp/github-detect")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Fatal(d.Execute())
 	},
 }
 
--- actor/github/pkg/detect/detect.go
+++ actor/github/pkg/detect/detect.go
@@ -1,15 +1,19 @@
 package detect
 
 import (
+	"context"
+	"encoding/json"
+	"io"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/takutakahashi/oci-image-operator/actor/github/pkg/github"
 )
 
 type Detect struct {
-	gh             *github.Github
-	outputFilePath string
+	gh *github.Github
+	w  io.Writer
 }
 
 func NewDetect(outputFilePath string) (*Detect, error) {
@@ -24,5 +28,39 @@ func NewDetect(outputFilePath string) (*Detect, error) {
 	if err := os.MkdirAll(filepath.Base(outputFilePath), 0644); err != nil {
 		return nil, err
 	}
-	return &Detect{gh: gh, outputFilePath: outputFilePath}, nil
+	f, err := os.Create(outputFilePath)
+	if err != nil {
+		return nil, err
+	}
+	return &Detect{gh: gh, w: f}, nil
+}
+
+func (d *Detect) Execute() error {
+	ctx := context.TODO()
+	branches, err := d.gh.BranchHash(ctx)
+	if err != nil {
+		return err
+	}
+	tags, err := d.gh.TagHash(ctx)
+	if err != nil {
+		return err
+	}
+	df := DetectFile{
+		Branches:      branches,
+		Tags:          tags,
+		LastUpdatedAt: time.Now(),
+	}
+	buf := []byte{}
+	if err := json.Unmarshal(buf, &df); err != nil {
+		return err
+	}
+	_, err = d.w.Write(buf)
+	return err
+
+}
+
+type DetectFile struct {
+	Branches      map[string]string `json:"branches"`
+	Tags          map[string]string `json:"tags"`
+	LastUpdatedAt time.Time         `json:"lastUpdatedAt"`
 }
